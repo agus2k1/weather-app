2df44d63c53eed4cfcefebffb7f74bfac34133a7
West <agus2kcoin@outlook.com>
2022-11-19 20:18:43 -0300

    Search component linked with Api

---
+++ src/Api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'c38a14167fmshd9ed68d9c71a01fp139d53jsn0bfc59735542',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-
+import "./App.css";
+import Search from "./components/search/search";
 
 function App() {
+
+  function handleOnSearchChange(searchData) {
+    console.log(searchData);
+  }
+
   return (
-    null
+    <div className="container">
+    <Search onSearchChange={handleOnSearchChange}/>
+    </div>
   );
 }
 
